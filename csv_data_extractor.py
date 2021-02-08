8692d45173abe56ed1b830959608aecf7a09c310
Jake Dibble <jdibble@bsu.edu>
2021-02-08 13:49:54 -0500

    Create boolean counter function

--- csv_data_extractor.py
+++ csv_data_extractor.py
@@ -9,14 +9,17 @@ class Extractor:
         self.turnList = []
         self.whiteRatingList = []
         self.blackRatingList = []
+        self.openingPlayList = []
+        self.ratedList = []
 
     def readFileToData(self):
         with open(self.csvFilename, newline='') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=',')
             for row in reader:
-                self.turnList.append(row['turns'])
+                self.turnList.append(int(row['turns']))
                 self.whiteRatingList.append(int(row['white_rating']))
                 self.blackRatingList.append(int(row['black_rating']))
+                self.ratedList.append(str(row['rated']))
     
     def getMin(self,list):
         return min(list)
@@ -25,18 +28,30 @@ class Extractor:
         return max(list)
 
     def getMean(self,list):
-        pass
-    def getMedian(list):
-        pass
-    def calculateStandardDev(list):
+        return statistics.mean(list)
+
+    def getMedian(self,list):
+        return statistics.median(list)
+
+    def calculateStandardDev(self,list):
         return statistics.stdev(list)
-        
+
+    def calculateBooleanFrequency(self,list):
+        trueNum = 0
+        falseNum = 0
+        for value in list:
+            if value == "TRUE":
+                trueNum += 1
+            if value == "FALSE":
+                falseNum += 1
+        return [trueNum,falseNum]
         
 
 extractor = Extractor('cs455_homework1_dibble.csv')
 extractor.readFileToData()
+print(extractor.calculateBooleanFrequency(extractor.ratedList))
 
-print(extractor.getMin(extractor.whiteRatingList))
+
         
     
 
