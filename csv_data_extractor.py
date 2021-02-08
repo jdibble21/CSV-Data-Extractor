459506e591ec59ab1477471b7c3e95cf598b4ca7
Jake Dibble <jdibble@bsu.edu>
2021-02-08 13:17:58 -0500

    Refactor row reading to ignore headers

--- csv_data_extractor.py
+++ csv_data_extractor.py
@@ -5,12 +5,17 @@ class Extractor:
     def __init__(self,csvfilename):
         self.csvFilename = csvfilename
         self.csvData = ""
+        self.turnList = []
+        self.whiteRatingList = []
+        self.blackRatingList = []
 
     def readFileToData(self):
         with open(self.csvFilename, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            reader = csv.DictReader(csvfile, delimiter=',')
             for row in reader:
-                print(row[0] + "  " + row[1])
+                self.turnList.append(row['turns'])
+                self.whiteRatingList.append(row['white_rating'])
+                self.blackRatingList.append(row['black_rating'])
 
 extractor = Extractor('cs455_homework1_dibble.csv')
 extractor.readFileToData()
