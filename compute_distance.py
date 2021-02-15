dd03061c5d9bda1ae5f2e0092c77baed0e4d215d
Jake Dibble <jdibble@bsu.edu>
2021-02-15 13:13:17 -0500

    Add compute distance script and hw3 dataset

---
+++ compute_distance.py
@@ -0,0 +1,10 @@
+# Choose 4 attributes from 100 rows, compute distance and normalize between values
+import csv
+
+class DistanceCalculator:
+    
+    def __init__(self, csvfilename):
+        self.csvFilename = csvfilename
+
+    def readFileToData(self):
+        pass
