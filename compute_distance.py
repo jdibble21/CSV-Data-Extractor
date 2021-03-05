460187cd570d35ad86bda87d8bd2c65384cf4674
Jake Dibble <jdibble@bsu.edu>
2021-03-05 15:28:19 -0500

    Refactor format

--- compute_distance.py
+++ compute_distance.py
@@ -1,5 +1,5 @@
 # Choose 4 attributes from 100 rows, normalize numeric vals and compute distance between row values
-
+from colorama import Fore, Style
 import pandas as pd
 import math
 
@@ -14,7 +14,7 @@ def normalizeDataframe(dataframe):
     return dataframe
 
 def calculateTwoRecordDistance(num1rec1,num1rec2,num2rec1,num2rec2,ordDistance,catDistance):
-    #example: index = 8,38 need num1[8] - num1[38] + num2[8] - num2[38] + ordDistance + catDistance
+    #example: index = 8,29 need num1[8] - num1[29] + num2[8] - num2[29] + ordDistance + catDistance
     distance = math.fabs(num1rec1 - num1rec2) + math.fabs(num2rec1 - num2rec2) + ordDistance + catDistance
     return distance * 0.25
 
@@ -36,7 +36,6 @@ def calculateOrdinalDistance(ordDataframe,d1,d2):
     record1Letter = record2[0]
     record1NumVal = float(record2[1:3])
     record2Distance = valDict[record1Letter] + (record1NumVal*0.001)
-
     return math.fabs(record1Distance - record2Distance)
         
 def calculateCategoricalDistance(catDataframe,d1,d2):
@@ -53,8 +52,10 @@ def calculateCategoricalDistance(catDataframe,d1,d2):
     record2 = valDict[catDataframe[d2]]
     return math.fabs(record1 - record2)
 
+
+
 R8 = 8
-R38 = 38
+R29 = 29
 R73 = 73
 
 pd.set_option('display.max_rows', None)
@@ -68,23 +69,48 @@ numericData2 = normalizeDataframe(numericData2)
 ordinalData = data['opening_eco']
 
 # FIRST COMPUTATION
-# Calculate Distance between record 8 and 38
-ordDistance = calculateOrdinalDistance(ordinalData,R8,R38)
-categoricalDistance = calculateCategoricalDistance(categoricalData,R8,R38)
-print("== FIRST COMPUTATION ==")
-print("Final Distance between record 8 and 38: ",calculateTwoRecordDistance(numericData1[R8],numericData1[R38],numericData2[R8],numericData2[R38],ordDistance,categoricalDistance),"\n")
+# Calculate Distance between record 8 and 29
+ordDistance = calculateOrdinalDistance(ordinalData,R8,R29)
+categoricalDistance = calculateCategoricalDistance(categoricalData,R8,R29)
+print("\n== FIRST COMPUTATION ==")
+print("Records 8 and 29\n")
+print("Normalized Record 8 Numeric Attribute 1 Value: ",numericData1[R8])
+print("Normalized Record 29 Numeric Attribute 1 Value: ",numericData1[R29])
+print("Normalized Record 8 Numeric Attribute 2 Value: ",numericData2[R8])
+print("Normalized Record 29 Numeric Attribute 2 Value: ",numericData2[R29])
+print("Ordinal Records Distance: ", ordDistance)
+print("Categorical Records Distance: ",categoricalDistance)
+print(Fore.YELLOW,"Final Distance: ",calculateTwoRecordDistance(numericData1[R8],numericData1[R29],numericData2[R8],numericData2[R29],ordDistance,categoricalDistance),"\n")
+print(Style.RESET_ALL)
 
 # SECOND COMPUTATION
-# Calculate Distance between record 38 and 73
-ordDistance = calculateOrdinalDistance(ordinalData,R38,R73)
-categoricalDistance = calculateCategoricalDistance(categoricalData,R38,R73)
-print("Final Distance between record 38 and 73: ",calculateTwoRecordDistance(numericData1[R38],numericData1[R73],numericData2[R38],numericData2[R73],ordDistance,categoricalDistance),"\n")
+# Calculate Distance between record 29 and 73
+ordDistance = calculateOrdinalDistance(ordinalData,R29,R73)
+categoricalDistance = calculateCategoricalDistance(categoricalData,R29,R73)
+print("== SECOND COMPUTATION ==")
+print("Records 29 and 73\n")
+print("Normalized Record 29 Numeric Attribute 1 Value: ",numericData1[R29])
+print("Normalized Record 73 Numeric Attribute 1 Value: ",numericData1[R73])
+print("Normalized Record 29 Numeric Attribute 2 Value: ",numericData2[R29])
+print("Normalized Record 73 Numeric Attribute 2 Value: ",numericData2[R73])
+print("Ordinal Records Distance: ", ordDistance)
+print("Categorical Records Distance: ",categoricalDistance)
+print(Fore.YELLOW,"Final Distance: ",calculateTwoRecordDistance(numericData1[R29],numericData1[R73],numericData2[R29],numericData2[R73],ordDistance,categoricalDistance),"\n")
+print(Style.RESET_ALL)
 
 # THIRD COMPUTATION
 # Calculate Distance between record 8 and 73
 ordDistance = calculateOrdinalDistance(ordinalData,R8,R73)
 categoricalDistance = calculateCategoricalDistance(categoricalData,R8,R73)
-print("Final Distance between record 8 and 73: ",calculateTwoRecordDistance(numericData1[R8],numericData1[R73],numericData2[R8],numericData2[R73],ordDistance,categoricalDistance),"\n")
-
+print("== THIRD COMPUTATION ==")
+print("Records 8 and 73\n")
+print("Normalized Record 8 Numeric Attribute 1 Value: ",numericData1[R8])
+print("Normalized Record 73 Numeric Attribute 1 Value: ",numericData1[R73])
+print("Normalized Record 8 Numeric Attribute 2 Value: ",numericData2[R8])
+print("Normalized Record 73 Numeric Attribute 2 Value: ",numericData2[R73])
+print("Ordinal Records Distance: ", ordDistance)
+print("Categorical Records Distance: ",categoricalDistance)
+print(Fore.YELLOW,"Final Distance: ",calculateTwoRecordDistance(numericData1[R8],numericData1[R73],numericData2[R8],numericData2[R73],ordDistance,categoricalDistance),"\n")
+print(Style.RESET_ALL)
 
 
